623040289b9be780e8bbb9e5449480aaf1a5c951
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-10-08 21:30:02 +1100

    Persisting our state with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyCsHquHIzdOG4zbIfIZYWUHIsgPkOMmuhc',
+  authDomain: 'react-catch-of-the-day-af612.firebaseapp.com',
+  databaseURL: 'https://react-catch-of-the-day-af612.firebaseio.com',
+});
+
+export default base;
